75d3b485d3733faa22f9219b1852424c768d550b
qxuewei <qxuewei@yeah.net>
2016-04-14 15:03:23 +0800

    初始化

---
+++ XWImagePickDemo/XWImagePickDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XWImagePickDemo
+//
+//  Created by 邱学伟 on 16/4/14.
+//  Copyright © 2016年 邱学伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XWImagePickDemo/XWImagePickDemo/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  XWImagePickDemo
+//
+//  Created by 邱学伟 on 16/4/14.
+//  Copyright © 2016年 邱学伟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+/**
+ *  选择图片监听方法
+ */
+- (IBAction)pickImageClick:(UIButton *)pickBtn {
+    NSLog(@"选择图片->");
+    
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
